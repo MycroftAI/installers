f0cd08be8b6000918526c7f6cbd48d665a67b7cf
Aditya Mehra <Aix.m@outlook.com>
2017-09-04 14:00:09 +0530

    Install Scripts

--- kde_plasmoid_fedora/plasmoid.sh
+++ kde_plasmoid_fedora/plasmoid.sh
@@ -36,41 +36,5 @@ sudo chmod +x /usr/share/plasma/plasmoids/org.kde.plasma.mycroftplasmoid/content
 # Install all necessary supporting libraries
 sudo dnf install dbus-python PyQt-devel sip sip-devel
 
-Arch=$(uname -m)
-
-if [ $Arch == 'i386' ]; then
-cp -R /usr/lib/python2.7/site-packages/dbus* /home/$USER/.virtualenvs/mycroft/lib/python2.7/site-packages/
-cp /usr/lib/python2.7/site-packages/_dbus* /home/$USER/.virtualenvs/mycroft/lib/python2.7/site-packages/
-cp -R /usr/lib/python2.7/site-packages/PyQt5* /home/$USER/.virtualenvs/mycroft/lib/python2.7/site-packages/    
-cp /usr/lib/python2.7/site-packages/sip* /home/$USER/.virtualenvs/mycroft/lib/python2.7/site-packages/
-fi
-
-if [ $Arch == 'x86_64' ]; then
-cp -R /usr/lib64/python2.7/site-packages/dbus* /home/$USER/.virtualenvs/mycroft/lib/python2.7/site-packages/
-cp /usr/lib64/python2.7/site-packages/_dbus* /home/$USER/.virtualenvs/mycroft/lib/python2.7/site-packages/
-cp -R /usr/lib64/python2.7/site-packages/PyQt5* /home/$USER/.virtualenvs/mycroft/lib/python2.7/site-packages/    
-cp /usr/lib64/python2.7/site-packages/sip* /home/$USER/.virtualenvs/mycroft/lib/python2.7/site-packages/
-fi
-
-# Get those awesome plasma skills
-git clone https://github.com/AIIX/krunner-search-skill  
-cp -R krunner-search-skill/* /opt/mycroft/skills/krunner-search-skill/
-git clone https://github.com/AIIX/plasma-activities-skill  
-cp -R plasma-activities-skill/* /opt/mycroft/skills/plasma-activities-skill/
-git clone https://github.com/AIIX/plasma-user-control-skill  
-cp -R plasma-user-control-skill/* /opt/mycroft/skills/plasma-user-control-skill/
-git clone https://github.com/AIIX/unsplash-wallpaper-plasma-skill  
-cp -R unsplash-wallpaper-plasma-skill/* /opt/mycroft/skills/unsplash-wallpaper-plasma-skill/
-git clone https://github.com/AIIX/clarifai-image-recognition-skill  
-cp -R clarifai-image-recognition-skill/* /opt/mycroft/skills/clarifai-image-recognition-skill/
-
-#Get those display skills for the desktop
-git clone https://github.com/AIIX/skill-weather
-cp -R skill-weather/* /opt/mycroft/skills/skill-weather/
-git clone https://github.com/AIIX/skill-stock
-cp -R skill-stock/* /opt/mycroft/skills/skill-stock/
-git clone https://github.com/AIIX/skill-wiki
-cp -R skill-wiki/* /opt/mycroft/skills/skill-wiki/
-
 # Restart the machine!
 echo "Everything is built and ready to go!"
