25d541b9cad32c7b3b932ad3617f07c8b38730e8
Aditya Mehra <Aix.m@outlook.com>
2017-09-04 11:46:06 +0530

    Install Scripts

---
+++ kde_plasmoid_debian/core_dev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Make sure we are in user's home directory
+cd ~
+cd mycroft-core
+
+echo "*****************************************************"
+echo "The compiling can take a long time, up to 2 hours."
+echo "Go enjoy a movie, we'll finish the install.       "
+echo "*****************************************************"
+./dev_setup.sh
+echo "-----------------------------------------------------"
+echo "Whew, finally finished that!"
+echo "-----------------------------------------------------"
---
+++ kde_plasmoid_fedora/core_build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Make sure we are in user's home directory
+cd ~
+
+echo "*****************************************************"
+echo "Installing git for pulling down source code"
+echo "*****************************************************"
+sudo dnf install git -y
+
+############################################################################
+# Retrieve and build Mycroft
+############################################################################
+
+# Pull down mycroft-core 
+git clone https://github.com/MycroftAI/mycroft-core.git
+
+# Build and install Mycroft: 
+cd mycroft-core
+
+echo "*****************************************************"
+echo "Getting Requirements."
+echo "*****************************************************"
+./build_host_setup_fedora.sh 
+echo "-----------------------------------------------------"
+echo "-----------------------------------------------------"
