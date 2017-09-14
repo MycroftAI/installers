79a59d7d633ac2557c394047c46fb30c297a4bfb
Aditya Mehra <aix.m@outlook.com>
2017-09-14 16:37:43 +0530

    Fix Missing Git Link

--- kde_plasmoid_debian/install.sh
+++ kde_plasmoid_debian/install.sh
@@ -43,6 +43,8 @@ echo "-----------------------------------------------------"
 # Retrieve and build the Plasmoid
 ############################################################################
 
+# Pull down the Plasmoid code from KDE repos
+git clone https://anongit.kde.org/plasma-mycroft.git
 
 # Install all necessary supporting libraries and tools
 sudo apt-get install libkf5notifications-data libkf5notifications-dev qml-module-qtquick2 qml-module-qtquick-controls2 qml-module-qtquick-controls qml-module-qtwebsockets qml-module-qt-websockets qtdeclarative5-qtquick2-plugin qtdeclarative5-models-plugin cmake cmake-extras cmake-data qml-module-qtquick-layouts libkf5plasma-dev extra-cmake-modules qtdeclarative5-dev build-essential g++ gettext libqt5webkit5 libqt5webkit5-dev libkf5i18n-data libkf5i18n-dev libkf5i18n5 -y
